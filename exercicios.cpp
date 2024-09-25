f6e8bc004b67a11f1ba3fe2b2eaee39317fd5360
ericaasn <155674437+ericaasn@users.noreply.github.com>
2024-09-24 21:47:51 -0300

    Update exercicios.cpp

--- exercicios.cpp
+++ exercicios.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int vet[10]; // cria um vetor de inteiros com 10 posições
+
+    // preenche o vetor com inteiros
+    for(int i = 0; i < 10; i++) {
+        cout << "Digite o valor da posição " << i << " do vetor: ";
+        cin >> vet[i];
+    }
+
+    // imprime o vetor
+    cout << "Vetor: ";
+    for(int i = 0; i < 10; i++) {
+        cout << vet[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+};
